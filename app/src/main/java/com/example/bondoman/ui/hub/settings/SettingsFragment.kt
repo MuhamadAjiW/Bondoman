39b1ccaa01bff71a2083098ae0134c9d38a422f8
MuhamadAjiW <16521119@mahasiswa.itb.ac.id>
2024-03-11 00:36:51 +0700

    feat: headers

--- app/src/main/java/com/example/bondoman/ui/hub/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/bondoman/ui/hub/settings/SettingsFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.example.bondoman.R
 import com.example.bondoman.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
@@ -18,5 +21,14 @@ class SettingsFragment : Fragment() {
     ): View? {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            val header = requireActivity().findViewById<TextView>(R.id.nav_header)
+            header.text = getString(R.string.hub_nav_settings)
+        }
     }
 }
