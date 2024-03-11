5f87d29424b427bda73b37032ae55c6eca938e5a
MuhamadAjiW <16521119@mahasiswa.itb.ac.id>
2024-03-11 17:47:42 +0700

    feat: screen orientation detection

--- app/src/main/java/com/example/bondoman/ui/hub/HubActivity.kt
+++ app/src/main/java/com/example/bondoman/ui/hub/HubActivity.kt
@@ -1,16 +1,21 @@
 package com.example.bondoman.ui.hub
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.bondoman.R
-import com.example.bondoman.databinding.ActivityHubBinding
 import com.example.bondoman.database.AppDatabase
+import com.example.bondoman.databinding.ActivityHubBinding
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomnavigation.LabelVisibilityMode
 
 class HubActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHubBinding
@@ -28,7 +33,16 @@ class HubActivity : AppCompatActivity() {
         // Initialize header
         binding.header.navBackButton.visibility = View.GONE
 
+        // TODO: Make orientation responsive
         // Initialize navbar and fragments
+        val orientation = resources.configuration.orientation
+        if(orientation == Configuration.ORIENTATION_LANDSCAPE){
+            println("Window is landscape")
+        }
+        else{
+            println("Window is portrait")
+        }
+
         val navView: BottomNavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         val appBarConfiguration = AppBarConfiguration(
@@ -41,5 +55,15 @@ class HubActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            println("Configuration Changed")
+
+        } else if(newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            recreate()
+        }
     }
 }
