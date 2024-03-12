c2d30c912c46ace7f389be348ebeb762fe35fc96
MuhamadAjiW <16521119@mahasiswa.itb.ac.id>
2024-03-12 10:40:58 +0700

    feat: finished settings ui

--- app/src/main/java/com/example/bondoman/ui/hub/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/bondoman/ui/hub/settings/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.bondoman.ui.hub.settings
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,8 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.example.bondoman.R
 import com.example.bondoman.databinding.FragmentSettingsBinding
+import com.example.bondoman.ui.hub.HubActivity
+import com.example.bondoman.ui.login.LoginActivity
 
 class SettingsFragment : Fragment() {
     private var _binding: FragmentSettingsBinding? = null
@@ -20,6 +23,11 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+
+        binding.buttonSaveTransaction.setOnClickListener(::saveTransaction)
+        binding.buttonSendTransaction.setOnClickListener(::sendTransaction)
+        binding.buttonLogout.setOnClickListener(::logout)
+
         return binding.root
     }
 
@@ -30,5 +38,24 @@ class SettingsFragment : Fragment() {
             val header = requireActivity().findViewById<TextView>(R.id.nav_title)
             header.text = getString(R.string.hub_nav_settings)
         }
+    }
+
+    //TODO: Implement
+    private fun saveTransaction(view: View){
+        println("Transaction saved")
+    }
+
+    //TODO: Implement
+    private fun sendTransaction(view: View){
+        println("Transaction sent")
+    }
+
+    //TODO: Implement
+    private fun logout(view: View){
+
+
+        val intent = Intent(requireContext(), LoginActivity::class.java)
+        startActivity(intent)
+        requireActivity().finish()
     }
 }
