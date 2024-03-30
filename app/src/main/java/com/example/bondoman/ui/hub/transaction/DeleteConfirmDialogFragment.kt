91dc515753403fb2f222fd7742cbddcf757b5a05
MuhamadAjiW <16521119@mahasiswa.itb.ac.id>
2024-03-30 23:50:12 +0700

    feat: transaction deletion

---
+++ app/src/main/java/com/example/bondoman/ui/hub/transaction/DeleteConfirmDialogFragment.kt
@@ -0,0 +1,28 @@
+package com.example.bondoman.ui.hub.transaction
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import com.example.bondoman.R
+
+class DeleteConfirmDialogFragment : DialogFragment() {
+    internal lateinit var listener: DeleteConfirmDialogListener
+
+    interface DeleteConfirmDialogListener{
+        fun onDeleteDialogPositiveClick(dialog: DialogFragment)
+        fun onDeleteDialogNegativeClick(dialog: DialogFragment)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+            builder
+                .setTitle(R.string.delete_confirm_dialog_label)
+                .setPositiveButton(R.string.delete_confirm_dialog_positive, DialogInterface.OnClickListener { _, index -> listener.onDeleteDialogPositiveClick(this)})
+                .setNegativeButton(R.string.delete_confirm_dialog_negative, DialogInterface.OnClickListener { _, index -> listener.onDeleteDialogNegativeClick(this)})
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+}
--- app/src/main/java/com/example/bondoman/ui/hub/transaction/TransactionAdapter.kt
+++ app/src/main/java/com/example/bondoman/ui/hub/transaction/TransactionAdapter.kt
@@ -1,28 +1,36 @@
 package com.example.bondoman.ui.hub.transaction
 
-import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bondoman.R
 import com.example.bondoman.database.entity.TransactionEntity
 import com.example.bondoman.databinding.ItemTransactionBinding
 import com.example.bondoman.ui.transaction.TransactionActivity
+import com.example.bondoman.viewmodel.transaction.TransactionViewModel
 import java.text.NumberFormat
 import java.util.Locale
 
 
 class TransactionAdapter(
-    private val ctx: Activity,
+    private val context: Context,
+    private val fragmentManager: FragmentManager,
+    private val viewModel: TransactionViewModel,
     private val tsList: List<TransactionEntity>
-): RecyclerView.Adapter<TransactionAdapter.ViewHolder>() {
+): RecyclerView.Adapter<TransactionAdapter.ViewHolder>(), DeleteConfirmDialogFragment.DeleteConfirmDialogListener {
     inner class ViewHolder(val view: ItemTransactionBinding): RecyclerView.ViewHolder(view.root)
+    private lateinit var transaction: TransactionEntity
     private lateinit var binding: ItemTransactionBinding
+    private val deleteConfirmDialog: DeleteConfirmDialogFragment = DeleteConfirmDialogFragment()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        binding = ItemTransactionBinding.inflate(LayoutInflater.from(ctx), parent, false)
+        binding = ItemTransactionBinding.inflate(LayoutInflater.from(context), parent, false)
+        deleteConfirmDialog.listener = this
         return ViewHolder(binding)
     }
 
@@ -44,11 +52,11 @@ class TransactionAdapter(
                 val gmapsIntentUri = Uri.parse("geo:46.414382,10.013988")
                 val mapIntent = Intent(Intent.ACTION_VIEW, gmapsIntentUri)
                 mapIntent.setPackage("com.google.android.apps.maps")
-                ctx.startActivity(mapIntent)
+                context.startActivity(mapIntent)
             }
 
             btnEdit.setOnClickListener {
-                val intent = Intent(ctx, TransactionActivity::class.java)
+                val intent = Intent(context, TransactionActivity::class.java)
                 intent.putExtra(TransactionActivity.actionKey, TransactionActivity.ACTION_EDIT)
 
                 // Pass all data
@@ -56,18 +64,31 @@ class TransactionAdapter(
                 intent.putExtra(TransactionActivity.transactionIdKey, tsList[position].id)
                 intent.putExtra(TransactionActivity.titleKey, tsList[position].title)
                 intent.putExtra(TransactionActivity.amountKey, tsList[position].amount)
-                intent.putExtra(TransactionActivity.categoryKey, ctx.resources.getStringArray(R.array.category_choices).indexOf(tsList[position].category))
+                intent.putExtra(TransactionActivity.categoryKey, context.resources.getStringArray(R.array.category_choices).indexOf(tsList[position].category))
                 intent.putExtra(TransactionActivity.locationKey, tsList[position].location)
                 intent.putExtra(TransactionActivity.timestampKey, tsList[position].timestamp)
 
-                ctx.startActivity(intent)
+                context.startActivity(intent)
+            }
+
+            btnDelete.setOnClickListener {
+                transaction = tsList[position]
+                deleteConfirmDialog.show(fragmentManager, "delete")
             }
         }
     }
 
-
-
     override fun getItemCount(): Int {
         return tsList.size
+    }
+
+    override fun onDeleteDialogPositiveClick(dialog: DialogFragment) {
+        println("Deleting data: " + transaction.id)
+        viewModel.delete(transaction)
+        deleteConfirmDialog.dismiss()
+    }
+
+    override fun onDeleteDialogNegativeClick(dialog: DialogFragment) {
+        deleteConfirmDialog.dismiss()
     }
 }
