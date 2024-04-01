459c7e074d868ce41de876a5f75463273e971bdc
Chiquita Ahsanunnisa <16521248@mahasiswa.itb.ac.id>
2024-04-01 17:59:19 +0700

    feat: encrypted shared preference

--- app/src/main/java/com/example/bondoman/services/SessionManager.kt
+++ app/src/main/java/com/example/bondoman/services/SessionManager.kt
@@ -2,12 +2,23 @@ package com.example.bondoman.services
 
 import android.content.Context
 import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import androidx.security.crypto.MasterKeys
 import com.example.bondoman.R
 
 class SessionManager (val context: Context) {
-    private val sharedPref = context.getSharedPreferences(
-        context.getString(R.string.app_name),
-        Context.MODE_PRIVATE
+    private val masterKey = MasterKey
+        .Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+        .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+        .build()
+
+    private val sharedPref = EncryptedSharedPreferences.create(
+        context,
+        "secret_shared_prefs",
+        masterKey,
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
     )
 
     companion object {
